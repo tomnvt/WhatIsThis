e9f760072ae1dda3df79bb5eeb1922c0b710cc99
“tomnvt” <tom.novota@gmail.com>
2018-05-09 20:49:51 +0200

    Add method for requesting extended Wikipedia query

--- WhatIsThis/Model/WikipediaQuery.swift
+++ WhatIsThis/Model/WikipediaQuery.swift
@@ -30,6 +30,30 @@ class WikipediaQuery {
                                             "redirects" : "1",
                                             "pithumbsize" : "500",
                                             "indexpageids" : ""]
+
+        if NetworkReachabilityManager()!.isReachable {
+
+            Alamofire.request(wikipediaURl, method: .get, parameters: parameters).responseJSON { (response) in
+                if response.result.isSuccess {
+                    let wikiJSON : JSON = JSON(response.result.value!)
+                    let pageid = wikiJSON["query"]["pageids"][0].stringValue
+                    self.queryResult = wikiJSON["query"]["pages"][pageid]["extract"].stringValue
+                    self.querySubject.onNext(self.queryResult)
+                }
+            }
+        } else {
+            queryResult = "No internet connection"
+        }
+    }
+    
+    func requestLongerInfo(result: String) {
+        let result = String(result.split(separator: "\n")[0]).lowercased()
+        let parameters : [String:String] = ["action" : "query",
+                                            "prop" : "extracts",
+                                            "meta" : "siteinfo",
+                                            "titles" : result,
+                                            "format" : "json",
+                                            "indexpageids" : ""]
         
         if NetworkReachabilityManager()!.isReachable {
             
@@ -47,4 +71,6 @@ class WikipediaQuery {
         }
     }
     
+
+
 }
