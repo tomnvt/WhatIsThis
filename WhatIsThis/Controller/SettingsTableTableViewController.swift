9bfdc12b36647ec3abedf3d1e040dad23f8eb533
“tomnvt” <tom.novota@gmail.com>
2018-06-07 20:23:34 +0200

    Convert settings table view data source into one dictionary

--- WhatIsThis/Controller/SettingsTableTableViewController.swift
+++ WhatIsThis/Controller/SettingsTableTableViewController.swift
@@ -11,13 +11,9 @@ import SnapKit
 
 class SettingsTableTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    private let titles = ["Speech synthesizer", "Classification Model"]
-    private var settings = ["Speech synthesizer: ON/OFF", "CoreML Model"]
-    private let models = CoreMLModels()
-    private let options = [["ON", "OFF"], CoreMLModels().modelNames]
+    private let settings = ["Speech Synthesis": ["ON", "OFF"], "Classification Model": CoreMLModels().modelNames]
     private let defaults = UserDefaults.standard
-    
-    let settingsTableView = SettingsTableView()
+    private let settingsTableView = SettingsTableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,25 +23,24 @@ class SettingsTableTableViewController: UIViewController, UITableViewDataSource,
         settingsTableView.tableView.dataSource = self
         settingsTableView.tableView.delegate = self
         
-        settings[0] = defaults.bool(forKey: "speechSynthesisIsOn") ? "Speech synthesizer: ON" : "Speech synthesizer: OFF"
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return settings.count
+        return settings.keys.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return options[section].count
+        return Array(settings.values)[section].count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return titles[section]
+        return Array(settings.keys)[section]
     }
 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
-        cell.textLabel!.text = options[indexPath.section][indexPath.row]
+        cell.textLabel!.text = Array(settings.values)[indexPath.section][indexPath.row]
         
         switch indexPath.section {
             
@@ -81,24 +76,21 @@ class SettingsTableTableViewController: UIViewController, UITableViewDataSource,
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch indexPath.section {
-            
         case 0:
-            switch options[indexPath.section][indexPath.row] {
-            case "ON":
+            switch indexPath.row {
+            case 0:
                 defaults.set(true, forKey: "speechSynthesisIsOn")
                 break
-            case "OFF":
+            case 1:
                 defaults.set(false, forKey: "speechSynthesisIsOn")
                 break
             default:
                 break
             }
             break
-            
         case 1:
             defaults.set(indexPath.row, forKey: "selecteModelNumber")
             break
-            
         default:
             break
         }
